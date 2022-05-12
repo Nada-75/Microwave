00fe1743210b230816311651c5fb40e871a86a9f
engRana404 <1901398@eng.asu.edu.eg>
2022-05-12 18:40:16 +0200

    upload the main.c file basic

---
+++ main.c
@@ -0,0 +1,30 @@
+#include "IO.h"
+enum state{Idle, cooking, beefWeight, chickenWeight, cookingTime, pause, end};
+int main(void){
+	int state = Idle;
+	while(1){
+		switch(state){
+			Idle:
+			//your code goes here
+			break;
+			cooking:
+			//your code goes here
+			break;
+			beefWeight:
+			//your code goes here
+			break;
+			chickenWeight:
+			//your code goes here
+			break;
+			cookingTime:
+			//your code goes here
+			break;
+			pause:
+			//your code goes here
+			break;
+			end:
+			//your code goes here
+			break;
+		}
+	}
+}
