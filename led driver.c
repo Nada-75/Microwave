e00fd2dff0e793f6b73441a326b474fa08cbd131
Sara556M <sararamadan20005@gmail.com>
2022-05-12 18:32:27 +0200

    Leds blink

--- led driver.c
+++ led driver.c
@@ -1,4 +1,6 @@
-
+#include "tm4c123gh6pm.h"
+#include "timer.h"
+#include "IO.h"
 void LED_INIT(unsigned char port_name ,unsigned char pin_number){
 	set_pinDirection (unsigned char port_name ,unsigned char pin_number,1);
  }
@@ -14,4 +16,11 @@ void LED_OFF(unsigned char port_name ,unsigned char pin_number){
 	
 void LED_TOG(unsigned char port_name ,unsigned char pin_number){
         TOG_pinData (unsigned char port_name ,unsigned char pin_number);
+	}
+
+// Blink Leds 
+void LED_TOG(unsigned char port_name ,unsigned char pin_number){
+        TOG_pinData (unsigned char port_name ,unsigned char pin_number);
+	      genericDelay(1000); //delay for 1 sec
+	      
 	}
