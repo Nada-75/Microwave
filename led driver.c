6be9eca42f2ee5f7c669bd11cfb348c6100db101
Nada Waleed598 <nadawaleed598@gmail.com>
2022-05-13 14:44:00 +0200

    modified led driver

--- led driver.c
+++ led driver.c
@@ -2,27 +2,25 @@
 #include "timer.h"
 #include "IO.h"
 
-void LED_INIT(unsigned char port_name ,unsigned char pin_number){
-	Set_pinDirection(port_name ,pin_number,1);
+void LED_INIT(){
+	GPIO_PORTF_Dir_R |= 0x0E;
  }
 
-
+//on led
 void ArrayLED_ON(){
 	GPIO_PORTF_DATA_R |= 0x0E;
 	}
-	
+//off led	
 void ArrayLED_OFF(){
 	GPIO_PORTF_DATA_R &= ~0x0E;
 	
-		//void LED_TOG(unsigned char port_name ,unsigned char pin_number){
-        TOG_BinData(port_name,bin_num); //Error here 
 	}
 
 // flash leds for 3 times  
 void ArrayLED_Flash(){
    for(int i=0;i<6;i++)
 		{
-	    GPIO_PORTF_DATA_R=GPIO_PORTF_DATA_R ^0x0E;
+        GPIO_PORTF_DATA_R=GPIO_PORTF_DATA_R ^0x0E;
     	genericDelay(1000); //1sec  
 		
 		} }
