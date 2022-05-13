5301c568bf7f1e76811c0e3a296822bb23c0830c
OmniaAhmed292 <omnyaahmed292@gmail.com>
2022-05-13 17:36:05 +0200

    edit variables in timer

--- timer.c
+++ timer.c
@@ -23,15 +23,19 @@ void SysTick_wait(unsigned long delay){
 
 //function to make a delay AND update the lcd 
 void statesDelay(unsigned long time){ //the function takes time in milliseconds
+	//used variables
 	int i;
-	unsigned int seconds = time/1000 ; //get the time in seconds 
-	unsigned int minutes = seconds/60; //get the minutes by dividing the whole seconde /60
+	int seconds;
+	int minutes;
+	//Equations
+	seconds	= time/1000 ; //get the time in seconds 
+	minutes = seconds/60; //get the minutes by dividing the whole seconde /60
 	seconds =seconds%60; //get the remaining seconds by taking the reminder of the previous operation ###
 	// now we have seconds and minutes in their variables
 	for(i=0; i<time;i++){
 		genericDelay(1000); //1 sec delay
 		//Update lcd goes here to be updated each sec with the new seconds and minutes
-		char *s ="%u : %u",minutes, seconds; //put s in the forman of min:sec in order to be printed ###
+		char *s ="%u : %u",minutes, seconds; //put s in the format of min:sec in order to be printed ###
 		lcd_send_string(s); //display the string 
 		if(seconds>0 && minutes>=0) {seconds--;} //decrease seconds each one second
 		if(seconds ==0 && minutes>0) { //If seconds reached zero, decrease the minutes 
@@ -50,7 +54,7 @@ void statesDelay(unsigned long time){ //the function takes time in milliseconds
 
 //A function to determine the delay for chicken and beef
 //this function will be called inside the genericDelay function to determine the time in seconds.
-unsigned long BC_delay(unsigned char state, unsigned char weight,unsigned long time ){
+unsigned long BC_delay(unsigned char state, int weight,unsigned long time ){
 	switch(state) {
 		case 'B': return time =30* weight*1000;       //in case of beef, delay = weight* 0.5 min
 		case 'C' : return time =12*weight*1000;       //in case of corn, delay= weight * 0.2 min 
@@ -63,7 +67,7 @@ return time=60*1000;	//60 sec dalay
 } 
 
 //A function to determine D(custom) delay 
-unsigned long D_delay (unsigned long time, unsigned char seconds, unsigned char minutes){ 
+unsigned long D_delay (unsigned long time, int seconds, int minutes){ 
 	 time = 60*minutes + seconds; //times in seconds
 	return time *1000; //as function takes milliseconds
 }
