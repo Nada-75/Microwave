dbf785203f6996af55ec8eb5e32df796781c7c1e
Nada Waleed598 <nadawaleed598@gmail.com>
2022-05-13 02:12:30 +0200

    add main2 and modfied led driver

--- led driver.c
+++ led driver.c
@@ -6,21 +6,29 @@ void LED_INIT(unsigned char port_name ,unsigned char pin_number){
  }
 
 
-void LED_ON(unsigned char port_name ,unsigned char pin_number){
-	writepin(unsigned char port_name ,unsigned char pin_number,1);
+void ArrayLED_ON(){
+	GPIO_PORTF_DATA_R |= 0x0E;
 	}
 	
-void LED_OFF(unsigned char port_name ,unsigned char pin_number){
-	writepin(unsigned char port_name ,unsigned char pin_number,0);
+void ArrayLED_OFF(){
+	GPIO_PORTF_DATA_R &= ~0x0E;
 	}
 	
-void LED_TOG(unsigned char port_name ,unsigned char pin_number){
+//void LED_TOG(unsigned char port_name ,unsigned char pin_number){
         TOG_pinData (unsigned char port_name ,unsigned char pin_number);
 	}
 
-// Blink Leds 
-void LED_TOG(unsigned char port_name ,unsigned char pin_number){
-        TOG_pinData (unsigned char port_name ,unsigned char pin_number);
-	      genericDelay(1000); //delay for 1 sec
-	      
+// flash leds for 3 times  
+void ArrayLED_Flash(){
+   for( i=0;i<6;i++)
+		{
+	    GPIO_PORTF_DATA_R=GPIO_PORTF_DATA_R ^0x0E;
+    	genericDelay(1000); //1sec  
+		
+		}
+// leds blink
+void ArrayLED_Blink(){
+ GPIO_PORTF_DATA_R=GPIO_PORTF_DATA_R ^0x0E;
+    	genericDelay(1000); //1sec 
+}	      
 	}
