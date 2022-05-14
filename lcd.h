c3efe58e93aadeccd1cf07b742b4712f13e48620
Nada Amin <104012796+Nada-Amin@users.noreply.github.com>
2022-05-14 22:33:55 +0200

    Update lcd.h

--- lcd.h
+++ lcd.h
@@ -1,16 +1,11 @@
+#define RS 0x01				 	//RS -> PB0 (0x01)
+#define EN 0x04  		 	 	//EN -> PB2 (0x04)
+#define lcd_clear 0x01
 #include "tm4c123gh6pm.h"
 
-#define DataPins GPIO_PORTA_DATA_R
-#define CTRLPins GPIO_PORTB_DATA_R
-//macros of CTRLPins
-#define E  0
-#define RS 1
-#define RW 2
-
 //Macros of commands
-#define ClrDisplay 0x01
-//#define Return_curs   0x02
-#define RShift_curs   0x06
+#define CLR_display   0x01
+#define RShiftCurs    0x06
 #define Turn_On       0x0F
 #define Curs_1stRow   0x80
 #define Curs_2ndRow   0xc0
@@ -18,10 +13,10 @@
 #define Set_8bit      0x38
 #define CursOff_DisON 0x0c
 
-//Functions Prototype
-void genericDelay(unsigned long time);
-void lcd_send_cmd(unsigned char cmd);
-void lcd_Init(void);
-void lcd_send_char(unsigned char chr);
+void delay_micro(int n);
+void LCD_Write4bits(unsigned char data, unsigned char control);
+void LCD4bits_Data(unsigned char data);
 void lcd_send_string(char *str);
-void lcd_move_curs(unsigned char row,unsigned char col);
+void LCD4bits_Cmd(unsigned char command);
+void LCD_Init(void);
+void systick_delay_msec(int t);
