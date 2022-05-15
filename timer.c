c2e55508f6244214bc1b12a30e0024e93e268fcc
OmniaAhmed292 <omnyaahmed292@gmail.com>
2022-05-15 09:28:49 +0200

    modified the statesDelay function

--- timer.c
+++ timer.c
@@ -24,46 +24,49 @@ void statesDelay(unsigned long time){ //the function takes time in seconds
 	//used variables
 	int i;
 	int seconds;
-	int minutes;	
-	LCD_Init(); //IF NOT IN MAIN
-	
-	//Equations
-	seconds	= time/1000 ; //get the time in seconds 
-	minutes = (int) seconds/60; //get the minutes by dividing the whole seconds /60
-	seconds = (int) seconds%60; //get the remaining seconds by taking the reminder of the previous operation ###
-	char s1;  //char s which will have the displayed value
-	char s2;
+	int minutes;
+	char s1,s2;  //char s1 and s2 which will have the displayed seconds
+	char m1,m2;	 //char m1 and m2 which will have the displayed minutes
 	//LCD_Init(); //IF NOT IN MAIN
-
 	
 	//Equations
-	seconds	= time/1000; //get the time in seconds 
-	minutes = seconds/60; //get the minutes by dividing the whole seconds /60
-	seconds = seconds%60; //get the remaining seconds by taking the reminder of the previous operation ###
+	minutes = time/60; //get the minutes by dividing the whole seconds /60
+	seconds = time%60; //get the remaining seconds by taking the reminder of the previous operation
 	
-	LCD4bits_Cmd(lcd_clear);
-	LCD4bits_Cmd(0x80);
-	// now we have seconds and minutes in their variables
+	//getting the four horsmen to be displayed
+
+	//preparing to display 
+	LCD4bits_Cmd(lcd_clear); //clear the lcd
+	LCD4bits_Cmd(0x80); //get the cmd at position 0
+	
+	// for each seond do that
 	for(i=0; i<time;i++){ 
 		 genericDelay(1000); //1 sec delay
+		
 		//Update lcd goes here to be updated each sec with the new seconds and minutes
 		
-		//LCD Display 
-		s1 = seconds+'0';
-		s2 = minutes+'0';
+		//update components of seconds after updating seconds and same for minutes
+		s2 = seconds/10; 
+		s1 = seconds%10;
+		m2 =minutes/10;
+		m1=minutes%10;
 		
-		LCD4bits_Data(s2);//display the string 
+		// we want the format to be m2 m1 : s2 s1
+		LCD4bits_Data(s2+'0'); //display the  second bit of minutes  
 		LCD4bits_Cmd(RShiftCurs);
-		LCD4bits_Data(':');
+		LCD4bits_Data(s2+'0'); //display the first bit of minutes 
 		LCD4bits_Cmd(RShiftCurs);
-		LCD4bits_Data(s1);
+		LCD4bits_Data(':'); // display : 
+		LCD4bits_Cmd(RShiftCurs);
+		LCD4bits_Data(s1+'0'); //display the second bit of seconds
+		LCD4bits_Cmd(RShiftCurs);
+		LCD4bits_Data(s1+'0'); //display the first bit of seconds
 		
-		//LCD4bits_Cmd(RShiftCurs);
 		
+		//Here goes the code of changing minutes and seconds every second.
 		
-		
-		/*if(seconds>0 && minutes>=0) {seconds--;} //decrease seconds each one second
->>>>>>> f0fee43d59ef42d5a323fda891c1fd65cf0ea2c5
+		if(seconds>0 && minutes>=0) {seconds--;} //decrease seconds each one second
+
 		if(seconds ==0 && minutes>0) { //If seconds reached zero, decrease the minutes 
 			// After 1:00 comes 0:59888888888//*****
 			minutes --;
@@ -73,10 +76,11 @@ void statesDelay(unsigned long time){ //the function takes time in seconds
 			//what to do when we finish goes here ###
 			break; //get out  from the for loop			
 			}
-		*/
+		
 		
 	}
 }
+
 
 //A function to determine the delay for chicken and beef
 //this function will be called inside the genericDelay function to determine the time in seconds.
@@ -98,5 +102,5 @@ return time=60;	//60 sec delay
 //A function to determine D(custom) delay 
 unsigned long D_delay ( int seconds, int minutes){ 
 	unsigned long time = 60*minutes + seconds; //times in seconds
-	return time; //as function takes milliseconds
+	return time; //as function takes seconds
 }
