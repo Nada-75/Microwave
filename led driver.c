5afa4824fafc3fc53ba6ef821de8fcac89b1a2e8
Sara556M <sararamadan20005@gmail.com>
2022-05-15 23:14:57 +0200

    edit led driver

--- led driver.c
+++ led driver.c
@@ -1,11 +1,19 @@
-//#include "tm4c123gh6pm.h"
+#include "tm4c123gh6pm.h"
 #include "timer.h"
-//#include "IO.h"
 #include "led driver.h"
-void LED_INIT(){
-	GPIO_PORTF_DIR_R |= 0x0E;
- }
 
+void LED_INIT(void){
+SYSCTL_RCGCGPIO_R|=0x20; //INTIALIZE THE CLOCK OF PORTF
+while((SYSCTL_PRGPIO_R & 0x20)==0); //delay
+GPIO_PORTF_LOCK_R = 0x4C4F434B; //unlocking the ports have the same value
+GPIO_PORTF_CR_R |= 0x0E; //Allow changing pin 3,2,1 in portF
+GPIO_PORTF_AMSEL_R &=~0x0E; //disable the analog function
+GPIO_PORTF_PCTL_R &=~0x0000FFF0;
+GPIO_PORTF_AFSEL_R &=~0x0E; //disable the alternate function
+GPIO_PORTF_DIR_R |= 0x0E; //Pin1,2,3 is output
+GPIO_PORTF_DEN_R |=0x0E;
+GPIO_PORTF_DATA_R &=~0x0E; //intialize pins 1,2,3 to be off
+}
 //on led
 void ArrayLED_ON(){
 	GPIO_PORTF_DATA_R |= 0x0E;
@@ -17,17 +25,18 @@ void ArrayLED_OFF(){
 	}
 
 // flash leds for 3 times  
-void ArrayLED_Flash(){
+	void ArrayLED_Flash(){
    int i;
    for(i=0;i<6;i++)
 		{
         GPIO_PORTF_DATA_R=GPIO_PORTF_DATA_R ^0x0E;
-    	genericDelay(1000); //1sec  
+    	genericDelay(500); //0.5sec  
 		
-		} }
+		} 
+}
 // leds blink
 void ArrayLED_Blink(){
- GPIO_PORTF_DATA_R=GPIO_PORTF_DATA_R ^0x0E;
+ 
+	GPIO_PORTF_DATA_R=GPIO_PORTF_DATA_R ^0x0E;
     	genericDelay(1000); //1sec 
 }	      
-	
--- led driver.h
+++ led driver.h
@@ -1,4 +1,4 @@
-void LED_INIT(unsigned char port_name ,unsigned char pin_number); //Initialize the led
+void LED_INIT(); //Initialize the led
 void ArrayLED_ON(); //leds on
 void ArrayLED_OFF(); //leds off
 void ArrayLED_Flash(); //leds flash
