0d6635d36eea4566b66592988a0722bfc1780d21
OmniaAhmed292 <omnyaahmed292@gmail.com>
2022-05-15 09:09:52 +0200

    edit delay functions

--- timer.c
+++ timer.c
@@ -3,7 +3,7 @@
 #include "IO.h"
 #include "lcd.h"
 
-//initialize the sys tic timer 
+//initialize the Systic timer 
 void SysTick_wait(unsigned long delay){ 
 	NVIC_ST_CTRL_R = 0x00;  
 	NVIC_ST_RELOAD_R = delay-1;  //Reload delay
@@ -15,30 +15,26 @@ void SysTick_wait(unsigned long delay){
 //A function to make a generic delay in seconds
  void genericDelay(unsigned long time){ 
 	int i;
- for(i=0;i<time ;i++) 
+ for(i=0;i<time ;i++) //time here in milli seconds 
  SysTick_wait(16000); //one millisecond delay 	
 }
 
 //function to make a delay AND update the LCD 
-void statesDelay(unsigned long time){ //the function takes time in milliseconds
+void statesDelay(unsigned long time){ //the function takes time in seconds
 	//used variables
 	int i;
 	int seconds;
-	int minutes;
-<<<<<<< HEAD
-	
+	int minutes;	
 	LCD_Init(); //IF NOT IN MAIN
 	
 	//Equations
 	seconds	= time/1000 ; //get the time in seconds 
 	minutes = (int) seconds/60; //get the minutes by dividing the whole seconds /60
 	seconds = (int) seconds%60; //get the remaining seconds by taking the reminder of the previous operation ###
-	char *s;  //char s which will have the displayed value
-=======
 	char s1;  //char s which will have the displayed value
 	char s2;
 	//LCD_Init(); //IF NOT IN MAIN
->>>>>>> f0fee43d59ef42d5a323fda891c1fd65cf0ea2c5
+
 	
 	//Equations
 	seconds	= time/1000; //get the time in seconds 
@@ -48,19 +44,9 @@ void statesDelay(unsigned long time){ //the function takes time in milliseconds
 	LCD4bits_Cmd(lcd_clear);
 	LCD4bits_Cmd(0x80);
 	// now we have seconds and minutes in their variables
-	//for(i=0; i<time;i++){
-		//genericDelay(3000); //1 sec delay
+	for(i=0; i<time;i++){ 
+		 genericDelay(1000); //1 sec delay
 		//Update lcd goes here to be updated each sec with the new seconds and minutes
-		
-<<<<<<< HEAD
-		sprintf(s,"%u : %u",minutes,seconds);//put s in the format of min:sec in order to be printed ###
-		//LCD Display 
-		LCD4bits_Cmd(lcd_clear);
-		LCD4bits_Cmd(0x80);
-		lcd_send_string(s); //display the string 
-		
-		if(seconds>0 && minutes>=0) {seconds--;} //decrease seconds each one second
-=======
 		
 		//LCD Display 
 		s1 = seconds+'0';
@@ -89,7 +75,7 @@ void statesDelay(unsigned long time){ //the function takes time in milliseconds
 			}
 		*/
 		
-	//}
+	}
 }
 
 //A function to determine the delay for chicken and beef
@@ -98,19 +84,19 @@ unsigned long BC_delay(unsigned char state, int weight){
 	unsigned long time;
 	switch(state) {
 		
-		case 'B': return time =30* weight*1000;       //in case of beef, delay = weight* 0.5 min
-		case 'C' : return time =12*weight*1000;       //in case of corn, delay= weight * 0.2 min 
+		case 'B': return time =30* weight;       //in case of beef, delay = weight* 0.5 min
+		case 'C' : return time =12*weight;       //in case of corn, delay= weight * 0.2 min 
 	}
 	 return 0; } //if we entered other inputs that B or C
 	 
 //A function to determine popcorn delay (just for further abstraction)	
 unsigned long A_delay()
 	{unsigned long time;
-return time=60*1000;	//60 sec delay
+return time=60;	//60 sec delay
 } 
 
 //A function to determine D(custom) delay 
 unsigned long D_delay ( int seconds, int minutes){ 
 	unsigned long time = 60*minutes + seconds; //times in seconds
-	return time *1000; //as function takes milliseconds
+	return time; //as function takes milliseconds
 }
