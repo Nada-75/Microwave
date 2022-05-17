3c6e2f2c40ba395867b69e817431a49afc7933ba
ReemHassan578 <104118223+ReemHassan578@users.noreply.github.com>
2022-05-17 21:59:32 +0200

    Update Keypad driver.h

--- Keypad driver.h
+++ Keypad driver.h
@@ -3,7 +3,6 @@
 //Functions Prototype
 void Keypad_init();
 unsigned char KeypadScan();
-unsigned int KeypadConversionWeight();
 
 void cookingtime_D(); //the function will take inputs for D, ddetermine minutes and seconds then display the countdown
 unsigned int KeypadConversionDigit(); //This function get the numeric input from keypad and return its integer value
