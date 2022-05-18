d934bbdd69cf9a0b2e4bc59d62ec91476baae584
Nada Amin <104012796+Nada-Amin@users.noreply.github.com>
2022-05-18 17:28:41 +0200

    update types

--- timer.h
+++ timer.h
@@ -1,8 +1,8 @@
 void SysTick_wait(unsigned long delay); //initialize the systic timer
 void genericDelay(unsigned long time);  //A function to make a generic delay in seconds
-unsigned long BC_delay(unsigned char state, int weight); // the delay for chicken and beef
-unsigned long A_delay(); //determine the delay for popcorn
-unsigned long D_delay (int seconds, int minutes);//A function to determine D(custom) delay
+int BC_delay(unsigned char state, int weight); // the delay for chicken and beef
+int A_delay(); //determine the delay for popcorn
+int D_delay (int seconds, int minutes);//A function to determine D(custom) delay
 
-void statesDelay(unsigned long time); ////function to make a delay AND update the lcd 
+void statesDelay(int time); ////function to make a delay AND update the lcd 
 void displayTime (int seconds, int minutes); //A function to display time in the wanted format
