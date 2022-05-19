28dcf64622696bc1e40327c51fd130bd6ffabd68
Nada Waleed598 <nadawaleed598@gmail.com>
2022-05-19 22:10:43 +0200

    Keypad modified

--- IO.h
+++ IO.h
@@ -9,3 +9,7 @@ void write_highnibble(unsigned char port_name,unsigned char data);
 void write_lownibble(unsigned char port_name,unsigned char data);
 void Set_portDir(unsigned char port_name,unsigned char direction);
 void TOG_BinData (unsigned char port_name, unsigned char bin_num);
+void Keypad_init();
+unsigned char KeypadScan();
+int KeypadConversionDigit();
+void cookingtime_D();
--- led driver.c
+++ led driver.c
@@ -43,4 +43,124 @@ void ArrayLED_Flash(){
 				genericDelay(500); //0.5sec  	
 		} 
 }
-	      
+	  /*
+
+//General port initialization function
+void Port_Init(unsigned char portname){
+	switch(portname)
+	{
+		
+		case 'C':
+		case 'c':
+		{
+			SET_BIT(SYSCTL_RCGCGPIO_R,2);
+			while(!(READ_BIT(SYSCTL_RCGCGPIO_R,2)));
+			GPIO_PORTC_LOCK_R=0X4C4F434B;
+			GPIO_PORTC_CR_R=0xFF;
+			GPIO_PORTC_DEN_R=0xFF;
+			break;
+		}
+		case 'E':
+		case 'e':
+		{
+			SET_BIT(SYSCTL_RCGCGPIO_R,4);
+			while(!(READ_BIT(SYSCTL_RCGCGPIO_R,4)));
+			GPIO_PORTE_LOCK_R=0X4C4F434B;
+			GPIO_PORTE_CR_R=0xFF;
+			GPIO_PORTE_DEN_R=0xFF;
+			break;
+		}
+		
+	}
+}
+//general pin direction function
+void Set_pinDirection (unsigned char port_name, unsigned char pin_num, unsigned char direction){ //set the bin in the wanted port to 1 or 0
+	switch (port_name){
+		
+		//C
+		case 'C':
+		case 'c': 
+		{if(direction ==1)
+		{SET_BIT(GPIO_PORTC_DIR_R, pin_num);
+		 break;
+		}
+		else
+			{CLR_BIT(GPIO_PORTC_DIR_R, pin_num);
+		 break;
+		} }	
+			//E
+		case 'E':
+		case 'e': 
+		{if(direction ==1)
+		{SET_BIT(GPIO_PORTE_DIR_R, pin_num);
+		 break;
+		}
+		else
+			{CLR_BIT(GPIO_PORTE_DIR_R, pin_num);
+		 break;
+		} }	
+	} 
+  }
+//function to enable the pull up for pin
+void enable_PullUP (unsigned char port_name, unsigned char pin_num){ //set the  pullup pin in the wanted port to 1 
+	switch (port_name){
+		
+	  //C
+		case 'C':
+		case 'c': 
+		{SET_BIT(GPIO_PORTC_PUR_R, pin_num);
+		 break;
+		}
+	} }
+
+
+
+//function to write the pin in any port
+void writePin(unsigned char portName,unsigned char pinNumber,unsigned char data)
+{
+	switch (portName)
+			
+{   case 'A' :
+		{if(data ==1)
+		{SET_BIT(GPIO_PORTA_DATA_R,pinNumber);
+		 break;
+		}
+		else
+			{CLR_BIT(GPIO_PORTA_DATA_R,pinNumber);
+		 break;
+		} 
+		}
+		
+		case 'E' :
+	  {if(data ==1)
+		{SET_BIT(GPIO_PORTE_DATA_R,pinNumber);
+		 break;
+		}
+		else
+			{CLR_BIT(GPIO_PORTE_DATA_R,pinNumber);
+		 break;
+		}  
+		}
+		
+}
+}
+unsigned char ReadPin (unsigned char portName,unsigned pinNum)
+{ while(1){
+
+		switch(portName)
+			
+{   case 'A' :
+	 return READ_BIT(GPIO_PORTA_DATA_R,pinNum);
+    case 'B' :
+	  return READ_BIT(GPIO_PORTB_DATA_R,pinNum);
+		case 'C' :
+	  return READ_BIT(GPIO_PORTC_DATA_R,pinNum);
+		case 'D' :
+	 return READ_BIT(GPIO_PORTD_DATA_R,pinNum);
+		case 'E' :
+	  return READ_BIT(GPIO_PORTE_DATA_R,pinNum);
+		case 'F' :
+	  return READ_BIT(GPIO_PORTF_DATA_R,pinNum);
+}
+}}
+*/    
