3409dd06109fad2df13e531787a145230c14e651
ReemHassan578 <reemhassan5523@gmail.com>
2022-05-21 13:56:17 +0200

    delete unwanted files

--- Keypad driver.h
+++
@@ -1,8 +0,0 @@
-#include "tm4c123gh6pm.h"
-
-//Functions Prototype
-void Keypad_init();
-unsigned char KeypadScan();
-
-void cookingtime_D(); //the function will take inputs for D, ddetermine minutes and seconds then display the countdown
-unsigned int KeypadConversionDigit(); //This function get the numeric input from keypad and return its integer value
